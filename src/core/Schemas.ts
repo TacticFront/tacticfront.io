5bd38450d33de617135105c52623f86401559979
TacticFront <tanner@tacticfront.io>
2025-06-10 17:23:34 -0600

    fix: enhance SafeString regex for improved validation and enforce maximum length

--- src/core/Schemas.ts
+++ src/core/Schemas.ts
@@ -140,7 +140,10 @@ export const TeamSchema = z.string();
 
 const SafeString = z
   .string()
-  .regex(/^\p{L}[\p{L}\p{N}\p{P}\s]{0,999}$/u, "Invalid");
+  .regex(
+    /^([a-zA-Z0-9\s.,!?@#$%&*()\-_+=\[\]{}|;:"'\/\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]|üÜ])*$/,
+  )
+  .max(1000);
 
 const PersistentIdSchema = z.string().uuid();
 const JwtTokenSchema = z.string().jwt();
